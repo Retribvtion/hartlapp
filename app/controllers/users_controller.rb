09e145eb7bbb7d44038dfd0b8b5f960cba977d4d
Retribvtion Rhino <sim2487@gmail.com>
2017-02-02 18:44:32 -0800

    Added create action, signup funtionality completed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "I can't believe you've done this."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
